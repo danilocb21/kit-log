a9f76205c82ca15718c14b289f95262ac0d5ecb3
Danilo <danilo.cbjp@outlook.com>
2026-06-23 11:48:45 -0300

    update benchmark

--- MLP/benchmark.sh
+++ MLP/benchmark.sh
@@ -1,16 +1,23 @@
-meson compile -C build
+#!/bin/bash
+make rebuild
 
 k=1
 
-for instance in data/instances/*; do
-	echo $instance >> ./output.txt
-	
-	echo "Processando $instance"
-	echo "Instance $k of 22"
-	
-	./build/src/mlp ${instance} >> ./output.txt
-	
-	k=$(($k+1))
-done
+echo "Instance,Time(s),Cost" > ./output.csv
 
-echo "-" >> ./output.txt
+for instance in data/instances/*; do
+    echo "Instance $k of 49"
+
+    output="$(basename "$instance")"
+    
+	echo "Processando $(basename $instance)"
+	
+	out_cpp=$(./mlp "${instance}")
+	out_csv="${out_cpp// /,}"
+	
+	output="${output},${out_csv}"
+    
+    echo "$output" >> ./output.csv
+    
+    k=$(($k+1))
+done
